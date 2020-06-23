8e350d8b46161697008a16bcfd3aec6cdbc8313e
Tran Thanh Mong <mongtt.tgg@vnpt.vn>
2020-06-23 09:24:11 +0700

    update environments/environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
   production: false,
   debug: true,
@@ -9,48 +13,77 @@ export const environment = {
       clientId: 'citizens-admin',
     },
     initOptions: {
-      onLoad: 'check-sso'
-    },
+      onLoad: 'check-sso',
+    }
   },
   routerConfig: {
-    enableTracing: false
+    enableTracing: false,
   },
   defaultRouterLink: 'home',
   insufficientPermissionRouterLink: 'error/insufficient-permission',
   apiProviders: [
     {
       name: 'digo-microservice',
-      rootUrl: 'https://digo-api-01.vnptigate.vn/',
+      rootUrl: 'https://digo-api.vnptigate.vn/',
       services: [
         {
           name: 'basedata',
-          path: 'ba'
+          path: 'ba',
         },
         {
           name: 'basecat',
-          path: 'bt'
+          path: 'bt',
         },
         {
           name: 'fileman',
-          path: 'fi'
+          path: 'fi',
         },
         {
           name: 'custom',
-          path: 'cu'
+          path: 'cu',
         },
         {
           name: 'human',
-          path: 'hu'
+          path: 'hu',
         },
         {
           name: 'postman',
-          path: 'po'
+          path: 'po',
         },
         {
           name: 'logman',
           path: 'lo'
+        },
+        {
+          name: 'surfeed',
+          path: 'su'
+        },
+        {
+          name: 'models',
+          path: 'v1',
+          rootUrl: 'https://digo-api.vnptigate.vn/modeling-service/'
+        },
+        {
+          name: 'rb-petition',
+          path: 'rb-petition'
         }
       ]
     }
-  ]
+  ],
+  mapbox: {
+    accessToken:
+      'pk.eyJ1IjoibW5oYWktbWFwYm94IiwiYSI6ImNrYmlvZ3d5bjBmdzcyem5xcWhnaDFrYjIifQ.tdsFG-l6QwZeKXDtO_r99g',
+  },
 };
+
+export const projectIdProcess = '3f9d9e1b-02cd-4e76-ad98-a67ab9830bf3';
+export const typeProcess = 'PROCESS';
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
